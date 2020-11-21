37442d23b4cd1e9410c1e206ba8403bd930b8a09
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-11-21 20:02:25 +0200

    :wastebasket: fix: removed dev level

--- constants/game.ts
+++ constants/game.ts
@@ -1,7 +1,6 @@
 export const TIME_LIMIT = 60
 
 export const DIFFICULTY_LEVELS = [
-  { label: 'dev', value: { cols: 2, rows: 2 } },
   { label: 'Easy: 4x3', value: { cols: 4, rows: 3 } },
   { label: 'Medium: 5x4', value: { cols: 5, rows: 4 } },
   { label: 'Hard: 6x4', value: { cols: 6, rows: 4 } }
